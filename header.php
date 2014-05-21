1e60b08ec31435aa61632f2870daa5c9e4da06d7
pradeepelcom <pradeep.elcom@gmail.com>
2014-05-21 17:13:28 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+        <meta charset="utf-8">
+		<!-- Site Title -->
+        <title>Robosapiens Technologies Pvt. Ltd.</title>
+    
+	
+        
+        <meta name="viewport" content="width=device-width">
+		<!-- Load Css -->
+		<link rel="stylesheet" type="text/css" href="assets/css/cssloader.css" />
+		<!-- Main Style -->
+			<link rel="stylesheet" type="text/css" href="assets/css/main.css"/>
+        <link rel="stylesheet" type="text/css" href="assets/css/shortcodes.css" />
+		<!-- Color -->
+		<link rel="stylesheet" type="text/css" href="assets/css/demo/color-picker.css">
+		
+		
+		
+        <link rel="stylesheet" href="assets/css/colorbox.css" />
+
+
+
+<!--End-->
+
+<!--[if lt IE 7]>
+	<link href="assets//css/font-awesome-ie7.min.css" rel="stylesheet">
+	<![endif]-->
+    
+<link href='http://fonts.googleapis.com/css?family=Patua+One' rel='stylesheet' type='text/css'>
+<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
+    <![endif]-->
+<!-- Le fav and touch icons -->
+<link rel="shortcut icon" href="assets/ico/favicon.ico">
+ <?php    
+    error_reporting(0);
+   
+  ?>
+  </head>
+      	
+<body>
+
+
+
+
+<nav>
+
+	<!-- ==================== Navbar ==================== -->
+    
+	<div class="navbar navbar-fixed-top">
+<!-- ==================== Top Menu Bar Start ==================== --> 
+<div style="width:100%; height:26px; background:#e1e1e1;">
+  <a href="http://www.robosapi.com/gc" target="_blank" style="outline:none; text-decoration:none;"><span style="font:Arial, Helvetica, sans-serif; font-size:13px; color:#000; margin-left:53px;"><i class="fa fa-certificate fa-lg"></i> Generate Certificate</span></a>
+  <a href="http://www.robosapi.com/certificate" target="_blank" style="outline:none; text-decoration:none;"><span style="font:Arial, Helvetica, sans-serif; font-size:13px; color:#000; margin-left:10px;"><i class="fa fa-unlock-alt fa-lg"></i> Verify Certificate</span></a>
+</div>     
+<!-- ==================== Top Menu Bar End ==================== -->    
+		<div class="navbar-inner from-top">
+        
+    
+        
+         
+		<div class="container">
+      		<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+		    
+           <div class="mainlogo"></div>
+            
+            
+            
+            
+            
+             
+		
+			<div class="nav-collapse collapse">
+				<ul class="nav pull-right">
+					<li><a href=" index.php"><i class="fa fa-home fa-lg"></i> Home</a></li>
+                    
+					<li class="dropdown"><a href="#"><i class="fa fa-globe fa-lg"></i> Services</a>
+						<ul class="dropdown-menu">
+							<li><a href=" #"><i class="fa fa-suitcase fa-lg"></i> Product Development</a></li>
+							
+                            <li class="dropdown"><a href=" #"><i class="fa fa-calendar fa-lg"></i> Event Management<i class="fa fa-angle-right fa-lg" style="margin-left:22px;"></i></a>
+                            	<ul class="dropdown-menu" style=" left:199px; top:0; border-radius:0; width:220px;">
+                        			<li><a href=" #"><i class="fa fa-magic fa-lg"></i> Air Show</a></li>
+									<li><a href=" #"><i class="fa fa-lightbulb-o fa-lg"></i> Sand Animation</a></li>
+									<li><a href=" #"><i class="fa fa-hand-o-up fa-lg"></i> TouchMagix</a></li>
+                                    <li><a href=" #"><i class="fa fa-unlink (alias) fa-lg"></i> Laser Show</a></li>
+									<li><a href=" #"><i class="fa fa-unlink (alias) fa-lg"></i> Magic Show</a></li>
+									<li><a href=" #"><i class="fa fa-expand fa-lg"></i> Artist Mgmt & Live</a></li>
+                                    <li><a href=" #"><i class="fa fa-calendar fa-lg"></i> Events</a></li>
+								</ul>
+                            </li>
+							<li><a href=" #"><i class="fa fa-group fa-lg"></i> Trainings</a></li>
+							<li><a href=" #"><i class="fa fa-pencil-square-o fa-lg"></i> Learning Workshops</a></li>
+                            <li><a href=" #"><i class="fa fa-bookmark-o fa-lg"></i> Certifications</a></li>
+                            <li><a href=" #"><i class="fa fa-file-text-o fa-lg"></i> Publishing House</a></li>
+                            
+						</ul>
+					</li>
+					<li class="dropdown"><a href="#"><i class="fa fa-cogs fa-lg"></i> Workshops</a>
+						<ul class="dropdown-menu">
+                        	<li class="dropdown"><a href=" #"><i class="fa fa-home fa-lg"></i> School Workshops <i class="fa fa-angle-right fa-lg" style="margin-left:22px;"></i></a>
+                            
+                            	<ul class="dropdown-menu" style=" left:199px; top:0; border-radius:0; width:220px;">
+                        			<li><a href=" #"><i class="fa fa-certificate fa-lg"></i> Summer Training</a></li>
+									<li><a href=" #"><i class="fa fa-cloud fa-lg"></i> Winter Training</a></li>
+									<li><a href=" #"><i class="fa fa-calendar-o fa-lg"></i> Diploma Courses</a></li>
+								</ul>
+
+                            </li>
+							<li class="dropdown"><a href=" #"><i class="fa fa-shield fa-lg"></i> College Workshop <i class="fa fa-angle-right fa-lg" style="margin-left:27px;"></i></a>
+                            	<ul class="dropdown-menu" style=" left:199px; top:0; border-radius:0; width:220px;">
+                        			<li><a href=" #"><i class="fa fa-angle-right fa-lg"></i> iRoboTics</a></li>
+									<li><a href=" #"><i class="fa fa-angle-right fa-lg"></i> Mobi-Robotics</a></li>
+									<li><a href=" #"><i class="fa fa-angle-right fa-lg"></i> BlueToot Robotics</a></li>
+                                    
+                                    <li><a href=" #"><i class="fa fa-angle-right fa-lg"></i> QuadCopter</a></li>
+									<li><a href=" #"><i class="fa fa-angle-right fa-lg"></i> Vision Robotics</a></li>
+									<li><a href=" #"><i class="fa fa-angle-right fa-lg"></i> Arduino</a></li>
+                                    
+                                    <li><a href=" #"><i class="fa fa-angle-right fa-lg"></i> Engine Design Workshop</a></li>
+									<li><a href=" #"><i class="fa fa-angle-right fa-lg"></i> MATLAB</a></li>
+									<li><a href=" #"><i class="fa fa-angle-right fa-lg"></i> 8051 Embedded Systems</a></li>
+                                    
+                                    <li><a href=" #"><i class="fa fa-angle-right fa-lg"></i> AVR Embedded Systems</a></li>
+									<li><a href=" #"><i class="fa fa-angle-right fa-lg"></i> Photoshop</a></li>
+									<li><a href=" #"><i class="fa fa-angle-right fa-lg"></i> Flash-o-mania</a></li>
+								</ul>
+                            
+                            </li>
+							<li><a href=" #"><i class="fa fa-qrcode fa-lg"></i> UpComing Workshops</a></li>
+						</ul>
+					</li>
+					<li class="dropdown"><a href=" #"><i class="fa fa-group fa-lg"></i> Training</a>
+                    	<ul class="dropdown-menu">
+                        	<li><a href=" #"><i class="fa fa-certificate fa-lg"></i> Summer Training</a></li>
+							<li><a href=" #"><i class="fa fa-cloud fa-lg"></i> Winter Training</a></li>
+							<li><a href=" #"><i class="fa fa-calendar-o fa-lg"></i> Diploma Courses</a></li>
+                            
+                            <li><a href=" #"><i class="fa fa-road fa-lg"></i> On Campus Training</a></li>
+                            <li><a href=" #"><i class="fa fa-external-link fa-lg"></i> Distance Learning</a></li>
+                            <li><a href=" #"><i class="fa fa-dot-circle-o fa-lg"></i> Online Courses</a></li>
+						</ul>
+                    
+                    </li>
+                    <li class="dropdown"><a  href=" #"><i class="fa fa-lightbulb-o fa-lg"></i> About</a>
+						<ul class="dropdown-menu">
+							<li><a href="workshop-list.php"><i class="fa fa-info-circle fa-lg"></i>Workshop List</a></li>
+							<li><a href="workshop-list-details.php"><i class="fa fa-group (alias) fa-lg"></i> Team at Robosapiens</a></li>
+							<li><a href=""><i class="fa fa-bullseye fa-lg"></i> Career</a></li>
+							<li><a href=" index.php/home/home/news_and_events"><i class="fa fa-file-text fa-lg"></i> News & Events</a></li>
+                            <li><a href=" index.php/home/home/awards_and_recognitions"><i class="fa fa-trophy fa-lg"></i> Awards & Recognitions</a></li>
+                            <li><a href=" index.php/home/home/media_coverages"><i class="fa fa-file-text-o fa-lg"></i> Media Coverages</a></li>
+                            <li><a href=" index.php/home/home/research_at_robosapiens"><i class="fa fa-flask fa-lg"></i> Research at Robosapiens</a></li>
+                            <li><a href=" index.php/home/home/clients"><i class="fa fa-user fa-lg"></i> Clients</a></li>
+                            <li><a href=" index.php/home/home/photo_gallery"><i class="fa fa-camera-retro fa-lg"></i> Photo Gallery</a></li>
+                            <li><a href=" index.php/home/home/video_gallery"><i class="fa fa-film fa-lg"></i> Video Gallery</a></li>
+							
+						</ul>
+                    </li>
+                    
+                    
+					<li><a href=" index.php/home/home/contact_us"><i class="fa fa-phone-square fa-lg"></i> Contact</a></li>
+                    
+                    <!--=======My Account=======--><!-- Search Form -->		
+					
+			  </ul>
+		  </div>
+		</div>
+		</div>
+		<div class="top-soc hidden-phone">
+			<div class="container">
+				<div class="row">
+					<div class="span6 from-right">
+					<ul class="address">
+						<li><span><i class="fa fa-phone"></i></span>+91 9990203445</li>
+						<li><span><i class="fa fa-envelope"></i></span>info@robosapi.com</li>
+					</ul>
+					</div>
+					<div class="span6 from-left">
+					<ul class="social text-right">
+                    	<li> <a href="#myModal" data-toggle="modal"><i class="fa fa-sign-in"></i></a></li>
+						<li><a class="fa fa-facebook" href="https://www.facebook.com/Robopedia" target="_blank"></a></li>
+                        <li><a class="fa fa-linkedin" href="#" target="_blank" ></a></li>
+                        <li><a class="fa fa-twitter" href="#" target="_blank" ></a></li>
+						
+					</ul>
+					</div>
+				</div>
+			</div>
+            
+		</div>
+        
+	</div>
+	<!-- ==================== /Navbar ==================== -->
+
+</nav>
+
+
+		
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
